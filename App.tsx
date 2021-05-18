c0984d0acdd82cf8e059003c4d86d75df0f894a3
israel <israel.pereira@dcx.ufpb.br>
2021-05-18 10:48:07 -0300

    configurando pag welcome⚙⚙

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './src/pages/welcome';
 
 // Tela de redirecionamento de páginas
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 
 import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
 
 export default function Welcome() {
   return (
-    <View >
-      <Text>
-        Gerencie suas plantas de forma fácil
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {'\n'}
+        suas plantas {'\n'}
+        de forma fácil
       </Text>
 
-      <Image source={wateringImg} />
+      <Image source={wateringImg} style={ styles.image}/>
 
-      <Text>
+      <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas.
         Nós cuidamos de lembrar você sempre que precisar.
       </Text>
 
-      <TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
         <Text>
           >
         </Text>
       </TouchableOpacity>
 
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -33,4 +36,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between'
   },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.heading,
+    marginTop: 38
+  },
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+  image: {
+    width: 292,
+    height:284
+  }
 });
